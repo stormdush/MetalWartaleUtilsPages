f7f6fd12a2458265d8967d579c0ef511230407ae
Avalon <stormdush@qq.com>
2018-04-18 17:30:23 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# MetalWartaleUtilsPages
-Pages fof MetalWartaleUtils
+# Vuetify Webpack SSR Template
+
+> Vuetify SSR Webpack Template
+
+## Build Setup
+
+``` bash
+vue init vuetifyjs/webpack-ssr
+
+cd webpack-ssr
+
+# npm
+npm install
+
+# yarn
+yarn
+```
+
+For additional information, please visit the [Official Documentation](https://vuetifyjs.com).
---
+++ pages/WelcomeView.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-app id="inspire" dark>
+    <v-navigation-drawer
+      clipped
+      fixed
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <v-list-tile @click="showLog">
+          <v-list-tile-action>
+            <v-icon>dashboard</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Dashboard</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="showTime">
+          <v-list-tile-action>
+            <v-icon>settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Settings</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app fixed clipped-left>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout justify-center align-center>
+
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer app fixed>
+      <span>&copy; 2018</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: true
+  }),
+  props: {
+    source: String
+  },
+  methods: {
+    showLog() {
+      console.log('>>>>>>>>>>>>>>>>>>>>>');
+    },
+    showTime() {
+      console.log(new Date());
+    }
+  }
+};
+</script>
